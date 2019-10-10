d258116f1c661ced5cd484517aebead5c70c5637
zq <robotmam.top@gmail.com>
2019-10-10 20:13:39 +0800

    #bean

---
+++ app/src/main/java/com/example/questionbank/bean/QuestionBean.java
@@ -0,0 +1,68 @@
+package com.example.questionbank.bean;
+
+public class QuestionBean {
+    int id;
+    String question;
+    String type;
+    String select_A;
+    String select_B;
+    String select_C;
+    String select_D;
+    String answer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getSelect_A() {
+        return select_A;
+    }
+
+    public void setSelect_A(String select_A) {
+        this.select_A = select_A;
+    }
+
+    public String getSelect_B() {
+        return select_B;
+    }
+
+    public void setSelect_B(String select_B) {
+        this.select_B = select_B;
+    }
+
+    public String getSelect_C() {
+        return select_C;
+    }
+
+    public void setSelect_C(String select_C) {
+        this.select_C = select_C;
+    }
+
+    public String getSelect_D() {
+        return select_D;
+    }
+
+    public void setSelect_D(String select_D) {
+        this.select_D = select_D;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
