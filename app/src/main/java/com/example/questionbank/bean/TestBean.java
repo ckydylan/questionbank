149531a321049a8445d4a705f467d515d595cd87
zq <robotmam.top@gmail.com>
2019-10-13 11:25:48 +0800

    #testbean

---
+++ app/src/main/java/com/example/questionbank/bean/TestBean.java
@@ -0,0 +1,40 @@
+package com.example.questionbank.bean;
+
+public class TestBean {
+    int id;
+    String data;
+    int qNum;
+    int wrongQuNum;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getqNum() {
+        return qNum;
+    }
+
+    public void setqNum(int qNum) {
+        this.qNum = qNum;
+    }
+
+    public int getWrongQuNum() {
+        return wrongQuNum;
+    }
+
+    public void setWrongQuNum(int wrongQuNum) {
+        this.wrongQuNum = wrongQuNum;
+    }
+}
